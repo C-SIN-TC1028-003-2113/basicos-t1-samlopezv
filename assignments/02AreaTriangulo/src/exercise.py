ee55162663615f0422856a513ec7005528101a9f
samlopezv <a01742394@itesm.mx>
2021-08-16 01:13:27 +0000

    Tarea 1: Programas que realizan cálculos

--- assignments/02AreaTriangulo/src/exercise.py
+++ assignments/02AreaTriangulo/src/exercise.py
@@ -1,6 +1,12 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    b = float(input('Dame la base: '))
+    h = float(input('Dame la altura: '))
+
+    area = b*h/2
+
+    print('El área es: '+str(area))
+
 
 if __name__ == '__main__':
     main()
--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    m1=float(input('Calificación de la materia: '))
+    m2=float(input('Calificación de la materia: '))
+    m3=float(input('Calificación de la materia: '))
+    m4=float(input('Calificación de la materia: '))
+
+    promedio=(m1+m2+m3+m4)/4
+
+    print('El promedio es: '+str(promedio))
 
 
 if __name__ == '__main__':
